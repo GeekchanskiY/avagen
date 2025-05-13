e9131d5efe5b42d701c8d5ad26750347d0da6606
GeekchanskiY <astrovskiy.04@gmail.com>
2025-05-13 11:27:27 +0300

    file structure updated

--- main.go
+++ main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
+	"log"
 	"time"
 
-	"github.com/GeekchanskiY/avagen/pkg"
+	"github.com/GeekchanskiY/avagen/pkg/scene"
 )
 
 func main() {
-	var width = 200
-	var height = 200
-	scene := pkg.NewScene(width, height)
-	scene.EachPixel(func(x, y int) color.RGBA {
-		return color.RGBA{
-			uint8(x * 255 / width),
-			uint8(y * 255 / height),
-			100,
-			255,
-		}
-	})
+	var (
+		start time.Time
 
-	scene.Save(fmt.Sprintf("./renders/%d.png", time.Now().Unix()))
+		width  = 200
+		height = 200
+		image  *scene.Scene
+
+		err error
+	)
+
+	start = time.Now()
+
+	image = scene.NewScene(
+		width,
+		height,
+		color.RGBA{
+			R: 0,
+			G: 255,
+			B: 255,
+			A: 255,
+		},
+		color.RGBA{R: 255, G: 255, B: 255, A: 255},
+	)
+
+	image.Clear()
+
+	log.Printf("Time passed to generate: %s", time.Since(start))
+
+	start = time.Now()
+
+	if err = image.Save("renders/test.png"); err != nil {
+		panic(err)
+	}
+
+	log.Printf("Time passed to save image: %s", time.Since(start))
 }
